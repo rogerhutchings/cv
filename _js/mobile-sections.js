6117740119cd74e5e134fb9f8feb30e1e6ba438d
Roger Hutchings <roger@incuna.com>
2014-03-19 14:47:17 +0000

    Add basic toggle function on mobile

---
+++ _js/mobile-sections.js
@@ -0,0 +1,28 @@
+(function() { 'use strict';
+
+    // Show / hide sections after tapping their titles for mobile devices
+
+    $(window).resize(function () {
+
+        // If we're on a horizontal iPhone or less, activate the show / hide toggling
+        // Check that body is the right thing to use - maybe window?
+        if ($('body').width() < 568) {
+
+            // Hide all section content by default
+            $('.js-section-content').hide();
+
+            // When we click a section title, toggle a sibling with the content class
+            $('.js-section-title').click(function () {
+                console.log('toggling');
+                $(this).siblings('.js-section-content').toggle();
+            });
+
+        } else {
+
+            // If we're on anything wider, show everything
+            $('.js-section-content').show();
+
+        }
+    });
+
+})();
