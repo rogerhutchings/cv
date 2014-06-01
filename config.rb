9571876797379831580dadb4536960d4ee124fb1
Roger Hutchings <roger@incuna.com>
2014-06-01 17:08:02 +0100

    Add Bower deps

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
-# Set this to the root of your project when deployed:
+add_import_path "./bower_components/reset-scss"
+add_import_path "./bower_components/modular-scale/stylesheets"
+
 http_path = "/"
 css_dir = "/"
 sass_dir = "_sass"
@@ -6,15 +8,7 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 fonts_dir = "fonts"
 
-# You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
 preferred_syntax = :sass
